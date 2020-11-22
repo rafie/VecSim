dda3154fe0979291664156a35c2010ef86be5eb3
meir@redislabs.com <meir@redislabs.com>
2020-11-22 09:48:38 +0200

    update scripts

--- add_data.py
+++ add_data.py
@@ -2,7 +2,7 @@ import redis
 import numpy as np
 conn = redis.Redis()
 p = conn.pipeline(transaction=False)
-for i in range(1000):
+for i in range(10000000):
     p.execute_command('RG.VEC_ADD', 'key%i' % i, np.random.rand(1, 128).astype(np.float32).tobytes())
     #p.execute_command('AI.TENSORSET', 'key%i' % i, "FLOAT", "1", "128", "BLOB", np.random.rand(1, 128).astype(np.float32).tobytes())
     if(i % 100 == 0):
--- query.py
+++ query.py
@@ -4,7 +4,7 @@ import numpy as np
 r = redis.Redis()
 blob = np.random.rand(1, 128).astype(np.float32)
 start = time.time()
-res = r.execute_command('RG.VEC_SIM', '4', '*', blob.tobytes())
+res = r.execute_command('RG.VEC_SIM', '4', blob.tobytes())
 end = time.time()
 for r in res[0]:
     print(r)
